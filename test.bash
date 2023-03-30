57772796981e65f3c42b7dbef7e6685d58563e2c
Leonard Pak <leopak2000@gmail.com>
2023-03-30 18:27:01 +0300

    leave only init sql script

--- test.bash
+++
@@ -1,4 +0,0 @@
-# Description: Test script for park
-docker image rm park
-docker build --no-cache -t park . \
-&&  docker run -it --rm --memory 2G --log-opt max-size=5M --log-opt max-file=3 --name park_perf -p 5000:5000 park
